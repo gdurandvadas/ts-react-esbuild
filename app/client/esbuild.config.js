c93a5298c40100ad8f7ac47b3b07336fcb5abf61
Gastón Durand Vadas <gaston.vadas@mollie.com>
2023-05-08 15:19:36 +0200

    Add error handling

--- app/client/esbuild.config.js
+++ app/client/esbuild.config.js
@@ -1,10 +1,13 @@
 const esbuild = require("esbuild");
 
-esbuild.context({
-  entryPoints: ["src/index.tsx"],
-  bundle: true,
-  minify: true,
-  outfile: "../server/public/client/bundle.js",
-}).then((ctx)=>{
-  ctx.watch()
-})
+esbuild
+  .context({
+    entryPoints: ["src/index.tsx"],
+    bundle: true,
+    minify: true,
+    outfile: "../server/public/client/bundle.js",
+  })
+  .then((ctx) => {
+    ctx.watch().catch((err) => console.error(err));
+  })
+  .catch((err) => console.error(err));
